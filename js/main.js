b8efcc02f84f45a66ad001efbeb421169ed2e10c
Tina Smith <smithtina1203@aol.com>
2025-05-21 14:52:13 -0500

    add more error handling

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(function () {
 
 
     $('#search').click(function (event) {
+        //debugger;
 
         event.preventDefault();
         $('.grid-container').empty();
@@ -10,25 +11,31 @@ $(function () {
         keyError.textContent = "";
         countError.textContent = "";
         searchError.textContent = "";
-        
+
         var keyword = $('#keyword').val();
         var count = $('#gifnumber').val();
+        const element = document.getElementById('keyword');
         count = Math.floor(count);
 
 
         if (keyword === null || keyword === "") {
             keyError.textContent = 'Keyword required';
             keyError.style.display = 'block';
+            element.style.border = '1px solid red';
+
+        }
+        else {
+            element.style.border = '1px solid lightgray';
         }
         if (keyword.length > 0) {
             if (count === null || count === "" || count < 1) {
-                
+
                 count = 1;
 
                 countError.textContent = 'Min is 1';
                 countError.style.display = 'block';
-
-            } else if (count > 32) {
+            }
+            else if (count > 32) {
 
                 countError.textContent = 'Max is 32';
                 countError.style.display = 'block';
@@ -42,22 +49,20 @@ $(function () {
 
         $.get(myUrl).done(function (response) {
 
-            if (response.data.length > 0)
-            {
+            if (response.data.length > 0) {
                 for (var i = 0; i < response.data.length; i++) {
                     var imageURL = '"' + response.data[i].images.preview_webp.url + '"';
                     $('.grid-container').append(`<img class="grid-img" src=${imageURL}>`);
                 }
             }
             else {
-                
-                searchError.textContent = `No GIFs found for ${keyword}`;
-                searchError.style.display = 'block';
+                if (keyword.length > 0) {
+                    searchError.textContent = `No GIFs found for ${keyword}`;
+                    searchError.style.display = 'block';
+                }
             }
-            
 
         });
-
 
     });
 
